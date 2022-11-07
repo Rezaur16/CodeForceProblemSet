137b61f2a966b16520da867f89c8aab7c47a7538
Rezaur16 <88153639+Rezaur16@users.noreply.github.com>
2022-11-07 22:41:27 +0600

    Add files via upload

---
+++ 4A.WaterMelon.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int w ;
+      cin>> w ;
+
+      if(w<=100 && w>=1 && w%2==0&&w!=2) {
+         cout<< "Yes";
+
+      }else{
+              cout<< "No";
+      }
+    }
+
